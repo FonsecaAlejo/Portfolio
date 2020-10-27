2a35a3eaacd25ebaf7dc151f6cf89dc34cf680d0
Alejo <alecjoc85@gmail.com>
2020-10-26 22:24:24 -0300

    Add section education and ability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PresentationComponent } from './components/presentation/presentation.component';
 import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { BiographyComponent } from './components/biography/biography.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PresentationComponent,
     WorkExperienceComponent,
+    EducationComponent,
+    BiographyComponent,
   ],
   imports: [
     BrowserModule,
